37a48d2bd153ea68ed35e9c1f0b369fe5ba2fa8e
Christian Cozma <christiancozma.work@gmail.com>
2021-06-03 12:38:23 -0500

    add to decks signup form and models.decks, added new file decks_form.py

--- flask-redux-start/app/api/decks_routes.py
+++ flask-redux-start/app/api/decks_routes.py
@@ -1,17 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.models import db, Deck, User, Character
-# from app.forms import NewDeckForm
+from app.forms import NewDeckForm
 
 decks_routes = Blueprint('decks', __name__, url_prefix="/api/decks")
 
+
+############* GET ALL DECKS ##########
 @decks_routes.route("/") #"/api/messages/"
 def decks():
     decks = Deck.query.all()
     return {"decks": [deck.to_dict() for deck in decks]}
 
+############* GET ONE ##########
 @decks_routes.route("/<int:id>")
 def deck(id):
     deck = Deck.query.get(id)
     return deck.to_dict()
 
+############* POST NEW DECK ##########
+@decks_routes.route("/", methods=["POST"])
+def create_new_deck():
+    def create_new_deck():
+        form = NewDeckForm()
+        form['csrf_token'].data = request.cookies['csrf_token']
 
+        if form.validate_on_submit():
+            data = form.data
+            newDeck = Deck (
+                deckName=data['deckName'],
+                category=data['category'],
+                userId=data["userId"]
+            )
