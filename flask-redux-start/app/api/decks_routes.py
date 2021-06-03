40c5c212a5d298c8751cc961ec5de5d1aece442e
Christian Cozma <christiancozma.work@gmail.com>
2021-06-03 15:25:09 -0500

    modifed decks_routes.py

--- flask-redux-start/app/api/decks_routes.py
+++ flask-redux-start/app/api/decks_routes.py
@@ -4,6 +4,16 @@ from app.forms import NewDeckForm
 
 decks_routes = Blueprint('decks', __name__, url_prefix="/api/decks")
 
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field}: {error}")
+    return errorMessages
+
 
 ############* GET ALL DECKS ##########
 @decks_routes.route("/") #"/api/messages/"
@@ -20,14 +30,38 @@ def deck(id):
 ############* POST NEW DECK ##########
 @decks_routes.route("/", methods=["POST"])
 def create_new_deck():
-    def create_new_deck():
         form = NewDeckForm()
         form['csrf_token'].data = request.cookies['csrf_token']
 
         if form.validate_on_submit():
             data = form.data
-            newDeck = Deck (
+            new_deck = Deck (
                 deckName=data['deckName'],
                 category=data['category'],
                 userId=data["userId"]
             )
+            db.session.add(new_deck)
+
+            db.session.commit()
+
+            return new_deck.to_dict()
+        return {"errors": validation_errors_to_error_messages(form.errors)}, 401
+
+@decks_routes.route("/<int:id>", methods=["POST"])
+def post_to_deck(id):
+    form = NewDeckForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+            data = form.data
+            add_to_deck = Deck.query.get(id) (
+                deckName=data['deckName'],
+                category=data['category'],
+                userId=data["userId"]
+            )
+            db.session.add(add_to_deck)
+
+            db.session.commit()
+
+            return add_to_deck.to_dict()
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 401
