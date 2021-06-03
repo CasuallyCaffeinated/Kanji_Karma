8ac2fedcaaefb770bd4ed35cb598dcd6f29d5517
Christian Cozma <christiancozma.work@gmail.com>
2021-06-03 11:03:52 -0500

    added the start of the various routes and modifed user routes by getting rid of login_required

--- flask-redux-start/app/api/user_routes.py
+++ flask-redux-start/app/api/user_routes.py
@@ -5,15 +5,15 @@ from app.models import User
 user_routes = Blueprint('users', __name__)
 
 
+# @login_required
 @user_routes.route('/')
-@login_required
 def users():
     users = User.query.all()
     return {"users": [user.to_dict() for user in users]}
 
 
+# @login_required
 @user_routes.route('/<int:id>')
-@login_required
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
