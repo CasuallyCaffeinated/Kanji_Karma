d43f383c37a7e63515f4c4d86a0c3151082c199b
Christian Cozma <christiancozma.work@gmail.com>
2021-06-03 18:40:32 -0500

    added validators to deck form for AddToDeck form

--- flask-redux-start/app/forms/decks_form.py
+++ flask-redux-start/app/forms/decks_form.py
@@ -1,7 +1,7 @@
 from werkzeug.utils import validate_arguments
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 
 class NewDeckForm(FlaskForm):
     deckName = StringField("deckName", validators=[DataRequired(),
@@ -11,5 +11,6 @@ class NewDeckForm(FlaskForm):
     submit = SubmitField("submit")
 
 class AddToDeck(FlaskForm):
-    characterId = IntegerField("characterId", validator=[DataRequired()])
+    characterId = IntegerField("characterId", validators=[DataRequired(),
+    NumberRange(min=1, max=2136, message="Kanji index in out of range! Could not find that kanji.")])
     # This might throw an error if I don't have a Submit field? check in the future
