f7b26cf909220db34988615d1e14a919259c9a16
Christian Cozma <christiancozma.work@gmail.com>
2021-06-03 22:53:59 -0500

    fixed syntax error

--- flask-redux-start/app/api/search_routes.py
+++ flask-redux-start/app/api/search_routes.py
@@ -6,10 +6,10 @@ search_routes = Blueprint('searches', __name__, url_prefix="/api/search")
 
 
 ####################! GET FOR ONE ####################
-@search_routes.route("<string:query>")
+@search_routes.route("/<string:query>")
 def find_one(query):
         searched = Character.query.filter \
         (Character.kanjiCharacter == query) \
         .first()
 
-        return searched.to_dict() 
+        return searched.to_dict()
