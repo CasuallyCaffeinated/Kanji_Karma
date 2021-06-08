ea21c9b79962d2ff3199539ebe4bba3efb2361ec
Christian Cozma <christiancozma.work@gmail.com>
2021-06-08 13:00:28 -0500

    modifed user reducer in store

--- flask-redux-start/react-app/src/store/session.js
+++ flask-redux-start/react-app/src/store/session.js
@@ -23,10 +23,10 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return;
     }
-    
+
     dispatch(setUser(data))
   }
-  
+
   export const login = (email, password) => async (dispatch)  => {
     const response = await fetch('/api/auth/login', {
       method: 'POST',
@@ -42,30 +42,31 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return data;
     }
-    
+
     dispatch(setUser(data))
     return {};
   }
-  
+
   export const logout = () => async (dispatch) => {
     const response = await fetch("/api/auth/logout", {
       headers: {
         "Content-Type": "application/json",
       }
     });
-    
+
     const data = await response.json();
     dispatch(removeUser());
   };
-  
-  
-  export const signUp = (username, email, password) => async (dispatch)  => {
+
+
+  export const signUp = (name, username, email, password) => async (dispatch)  => {
     const response = await fetch("/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        name,
         username,
         email,
         password,
@@ -75,7 +76,7 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
         return data;
     }
-    
+
     dispatch(setUser(data))
     return {};
   }
