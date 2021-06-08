3ef4d96c923e57f9f815c3129f26db790a9a42d1
Christian Cozma <christiancozma.work@gmail.com>
2021-06-08 12:57:52 -0500

    modifed auth-routes in backend in order to take in a name column

--- flask-redux-start/app/api/auth_routes.py
+++ flask-redux-start/app/api/auth_routes.py
@@ -64,6 +64,7 @@ def sign_up():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         user = User(
+            name=form.data['name'],
             username=form.data['username'],
             email=form.data['email'],
             password=form.data['password']
