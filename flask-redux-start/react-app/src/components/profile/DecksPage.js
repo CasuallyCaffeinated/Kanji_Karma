c8657feee18389ddf4f2666721747f7c97aa8519
Christian Cozma <christiancozma.work@gmail.com>
2021-06-11 12:38:19 -0500

    GET working on the front end! User can see their decks

--- flask-redux-start/react-app/src/components/profile/DecksPage.js
+++ flask-redux-start/react-app/src/components/profile/DecksPage.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import { getDecksThatBelongToUser } from "../../store/users"
 
-
+import { Flex, Box } from "@chakra-ui/react"
 
 function DecksPage() {
-    return (
-        <div>
 
-        </div>
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.userReducer)
+    const { id } = useParams()
+
+    useEffect(() => {
+
+        dispatch(getDecksThatBelongToUser(id))
+    }, [dispatch])
+
+    return (
+        <Flex h="100vh" w="100vw" align="flex-start" justify="center" flexGrow="1">
+            <Flex h="95%"
+             w="90%"
+             flexGrow="1"
+             bgColor="blackAlpha.200"
+             rounded="lg"
+             align="center"
+             justify="center"
+             >
+                 {
+                     Object.values(user).map((singleUser, idx) =>
+                     <Flex
+                     justify="space-evenly"
+                     align="center"
+                     key={idx}
+
+                     >
+                        {
+                            singleUser?.decks.map((deck, index) => {
+                                return <Box margin="10px" key={index}>{deck.deckName}</Box>
+                            })
+                        }
+                     </Flex>
+                     )
+                 }
+            </Flex>
+        </Flex>
     )
 }
 
