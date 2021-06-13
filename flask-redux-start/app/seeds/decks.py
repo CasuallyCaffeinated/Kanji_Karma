3139c8acfcb199895c52621599c0ba177aceb865
Christian Cozma <christiancozma.work@gmail.com>
2021-06-13 12:22:40 -0500

    big chnage: seeded all Jouyou kanji! After testing it, it works

--- flask-redux-start/app/seeds/decks.py
+++ flask-redux-start/app/seeds/decks.py
@@ -3,10 +3,13 @@ from app.models import db, Deck
 
 def seed_decks():
 
-    food_vocab = Deck(deckName="Food vocab", category="Food", userId=1)
+    food_vocab = Deck(deckName="Food", category="Food vocabulary", userId=1)
     db.session.add(food_vocab)
 
-    weather_vocab = Deck(deckName="Weather vocab", category="Weather", userId=2)
+    music_vocab = Deck(deckName="Music", category="Music vocabulary", userId=1)
+    db.session.add(music_vocab)
+
+    weather_vocab = Deck(deckName="Weather", category="Weather", userId=2)
     db.session.add(weather_vocab)
 
     db.session.commit()
